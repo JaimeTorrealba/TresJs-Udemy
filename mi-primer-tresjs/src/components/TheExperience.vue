e9a752ebe33c83350ef8928a2cefd5c59c372f95
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-05-19 14:25:56 +0100

    :memo: contexto

--- mi-primer-tresjs/src/components/TheExperience.vue
+++ mi-primer-tresjs/src/components/TheExperience.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
+// import { shallowRef, watch } from 'vue'
 import { TresCanvas  } from '@tresjs/core'
+import TheBox from './componentes/TheBox.vue'
+
 </script>
 
 <template>
   <TresCanvas window-size>
     <TresPerspectiveCamera :position="[0, 3, 5]" :look-at="[0,0,0]"  />
-    <TresMesh >
-        <TresBoxGeometry :args="[1,1,1]" />
-        <TresMeshBasicMaterial color="green" />
-      </TresMesh>
+    <TheBox />
   </TresCanvas>
 </template>
---
+++ mi-primer-tresjs/src/components/componentes/TheBox.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useTresContext } from '@tresjs/core'
+
+const ctx = useTresContext()
+console.log('jaime ~ ctx:', ctx);
+</script>
+<template>
+    <TresMesh ref="boxRef">
+        <TresBoxGeometry :args="[1,1,1]" />
+        <TresMeshBasicMaterial color="green" />
+      </TresMesh>
+</template>
