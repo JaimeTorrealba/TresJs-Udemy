d378ce0dc75e76ee9d47111326e6ce762c01e4ba
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-05-23 13:56:32 +0100

    :memo: shadows

---
+++ pbr/src/components/earth/TheEarth.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { shallowRef } from 'vue';
+import { useLoop } from '@tresjs/core'
+
+const sphereRef = shallowRef(null)
+
+const { onBeforeRender } = useLoop()
+
+onBeforeRender(({elapsed}) => {
+    sphereRef.value.position.z = Math.sin(elapsed)
+})
+
+</script>
+
+<template>
+    <TresMesh ref="sphereRef" cast-shadow >
+        <TresSphereGeometry :args="[1, 16]" />
+        <TresMeshStandardMaterial />
+    </TresMesh>
+    <TresMesh receive-shadow :rotation-x="-Math.PI * 0.5" :position-y="-2" >
+        <TresPlaneGeometry :args="[10, 10]" />
+        <TresMeshStandardMaterial color="#f7f7f7" />
+    </TresMesh>
+    
+</template>
