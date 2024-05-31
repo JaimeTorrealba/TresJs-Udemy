ec6dd77ca30c3b90cf3e444b4b690ab68bc47744
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-05-31 11:13:39 +0100

    :sparkles: paneles de configuracione

--- section-4/src/components/TheExperience.vue
+++ section-4/src/components/TheExperience.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
 import { OrbitControls } from '@tresjs/cientos'
-import SelectableGrid from './classes/SelectableGrid.vue'
+import TheTweaks from './classes/TheTweaks.vue'
 </script>
 
 <template>
     <TresCanvas clear-color="#333" window-size>
         <TresPerspectiveCamera :position="[0, 10, 10]" :look-at="[0, 0, 0]" />
-        <SelectableGrid />
+        <TheTweaks />
         <OrbitControls />
     </TresCanvas>
 </template>
---
+++ section-4/src/components/classes/TheTweaks.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { reactive } from 'vue';
+import {Pane} from 'tweakpane';
+
+const pane = new Pane();
+
+const object = reactive({
+    x: 0,
+    width: 1
+    // y: 0,
+    // z: 0
+});
+
+pane.addBinding(object, 'x', {
+    min: -10,
+    max: 10,
+    step: 1,
+});
+pane.addBinding(object, 'width', {
+    min: -10,
+    max: 10,
+    step: 0.1,
+});
+</script>
+<template>
+<TresMesh :position="[object.x, 0, 0]">
+    <TresBoxGeometry :args="[object.width, 1, 1]" />
+    <TresMeshNormalMaterial  />
+</TresMesh>
+</template>
