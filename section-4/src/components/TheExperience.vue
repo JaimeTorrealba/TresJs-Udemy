340900e7d9996453ef6f26caf944073ce63e714f
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-07-31 04:04:49 +0100

    :sparkles: transition component

--- section-4/src/components/TheExperience.vue
+++ section-4/src/components/TheExperience.vue
@@ -1,13 +1,44 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { TresCanvas } from '@tresjs/core'
 import { OrbitControls } from '@tresjs/cientos'
-import TheTweaks from './classes/TheTweaks.vue'
+import { gsap } from 'gsap'
+
+const showBox = ref(false)
+
+const onEnter = (el, done) => {
+    gsap.from(el.material, { duration: 1, opacity: 0, onComplete: done })
+    gsap.from(el.position, { duration: 1, y: -1 })
+}
+const onLeave = (el, done) => {
+    gsap.to(el.material, { duration: 1, opacity: 0 })
+    gsap.to(el.position, { duration: 1, y: -1 })
+}
 </script>
 
 <template>
+    <button class="show-box" @click="showBox = !showBox">Toggle Box</button>
     <TresCanvas clear-color="#333" window-size>
         <TresPerspectiveCamera :position="[0, 10, 10]" :look-at="[0, 0, 0]" />
-        <TheTweaks />
+        <Transition @enter="onEnter" @leave="onLeave" :css="false">
+            <TresMesh v-if="showBox">
+                <TresBoxGeometry :args="[1, 1, 1]" />
+                <TresMeshNormalMaterial transparent />
+            </TresMesh>
+        </Transition>
         <OrbitControls />
     </TresCanvas>
 </template>
+<style scoped>
+.show-box {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    z-index: 100;
+    padding: 10px;
+    background: #333;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+</style>
