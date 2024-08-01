983bb6dd3f073cf6849c09ac6ba8c5ace60ce577
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-08-01 16:56:09 +0100

    :sparkles: explorando cientos

---
+++ section-5/src/components/TheExperience.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+import TheCientos from './classes/TheCientos.vue'
+
+</script>
+<template>
+    <TresCanvas window-size clear-color='#111'>
+        <TresPerspectiveCamera :position='[0, 0, 3]' :fov='45' :aspect='1' :near='0.1' :far='1000' />
+        <OrbitControls />
+        <Suspense>
+            <TheCientos />
+        </Suspense>
+    </TresCanvas>
+</template>
---
+++ section-5/src/components/classes/TheCientos.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Sky, Box, HolographicMaterial } from '@tresjs/cientos'
+</script>
+<template>
+    <Box :args="[2, 1, 3]">
+        <HolographicMaterial />
+    </Box>
+    <!-- <TresMesh>
+        <TresBoxGeometry :width='1' :height='1' :depth='1' />
+    </TresMesh> -->
+    <Sky />
+</template>
