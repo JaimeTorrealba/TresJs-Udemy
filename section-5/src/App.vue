61c9433c670f5d43a6235d8db0bcafab4a6398fa
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-09-12 16:23:06 +0100

    add simple page template hero

--- section-5/src/App.vue
+++ section-5/src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
 import TheExperience from './components/TheExperience.vue'
+import MyPage from './components/MyPage.vue'
 </script>
 
 <template>
   <TheExperience />
+  <MyPage />
 </template>
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ section-5/src/components/MyPage.vue
@@ -0,0 +1,62 @@
+<script setup>
+</script>
+<template>
+    <header>
+        <nav class="navbar">
+            <p>Logo</p>
+            <ul class="menu">
+                <li><a class="item" href="#">Home</a></li>
+                <li><a class="item" href="#">About</a></li>
+                <li><a class="item" href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main">
+        <h1>My Page</h1>
+        <p>Welcome to my page</p>
+    </main>
+    <footer class="footer">
+        <p>© 2021 My Company</p>
+    </footer>
+</template>
+<style scoped>
+/* HEADER */
+
+.navbar {
+    background-color: #333;
+    color: white;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.menu {
+    display: flex;
+    list-style: none;
+}
+
+.item {
+    padding: 0 1rem;
+    color: white;
+    text-decoration: none;
+}
+
+
+/* MAIN */
+
+.main {
+    padding: 2rem;
+    text-align: center;
+    min-height: 100vh;
+}
+
+/* FOOTER */
+
+.footer {
+    background-color: #333;
+    color: white;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+}
+</style>
